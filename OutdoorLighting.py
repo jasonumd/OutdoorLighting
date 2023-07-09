a7cda23429ebfb5a4fb2b3dbec00888f6aeef5ce
Jason A. Evans <jasonevans77@gmail.com>
2023-07-09 12:11:38 -0400

    Update OutdoorLighting.py

--- OutdoorLighting.py
+++ OutdoorLighting.py
@@ -1,9 +1,15 @@
 # Using the following cron to run this script at 2 am UTC everyday and on system reboot (crontab -e):
 # 0 2 * * * /home/pi/OutdoorLighting/OutdoorLighting.py
+# @reboot /home/pi/OutdoorLighting/LightsOff.py
 # @reboot /home/pi/OutdoorLighting/OutdoorLighting.py
 
-# Set the pi to utilize UTC.
-# install pip3, python-requests, python-dateutil
+# Set the pi to utilize UTC (sudo raspi-config, Localisation Options, Timezone, None of the Above, UTC)
+# Enable GPIO pins (sudo raspi-config, Interface Options, I2C, Yes) (sudo raspi-config, Interface Options, SPI, Yes)
+# sudo apt-get install python3-pip
+# sudo apt-get install at
+# pip3 install requests
+# pip3 install python-dateutil
+# pip3 install pytz
 # https://www.raspberrypi.org/documentation/linux/usage/cron.md
 # https://crontab.guru/#0_19_*_*_*
 # https://tecadmin.net/one-time-task-scheduling-using-at-commad-in-linux/
@@ -21,7 +27,8 @@ from pytz import timezone
 import os
 
 BufferTimeMinutes = 10
-LightingGPIOPin = 7
+myCmdLightsOn = 'python3 /home/pi/OutdoorLighting/LightsOff.py'
+myCmdLightsOff = 'python3 /home/pi/OutdoorLighting/LightsOff.py'
 
 #Step 1: Get now.
 nowUtc = datetime.utcnow() #Could also use now() as the pi will operate in UTC.
@@ -30,7 +37,7 @@ print('nowUtc: ' + str(nowUtc))
 
 #Step 2: Get API (UTC) sunrise/sunset times at specified latitude/longitude.
 #Going to work strictly in UTC.
-response = requests.get('https://api.sunrise-sunset.org/json?lat=39.558003&lng=-76.352503&formatted=0')
+response = requests.get('https://api.sunrise-sunset.org/json?lat=39.558003&lng=-76.352503&formatted=0', verify=False)
 apidata = response.json()
 results = apidata['results']
 sunriseApiRawUtc = results['sunrise']
@@ -56,24 +63,17 @@ sunsetLightsOnUtc = sunsetUtc - timedelta(minutes = BufferTimeMinutes)
 print('sunriseLightsOffUtc: ' + str(sunriseLightsOffUtc))
 print('sunsetLightsOnUtc: ' + str(sunsetLightsOnUtc))
 
-#Step 3: Set pin to write status. Not sure if it changes with reboot.
-mycmd = 'gpio mode ' + str(LightingGPIOPin) + ' out'
-os.system(mycmd)
-
 #Step 4: Turn lights on if the system is rebooted during period when they should be on.
 if (nowUtc < sunriseLightsOffUtc) or (nowUtc > sunsetLightsOnUtc): #Turn lights on, need to set turning them on/off. Standard case of the 1 am daily cron.
-	print('a')
-	mycmd = 'gpio write ' + str(LightingGPIOPin) + ' 1' #Turn lights on
-	os.system(mycmd)
+	os.system(myCmdLightsOn)
 
 #Step 5: Set future off/on in UTC.
 fmt = '%H:%M %Y-%m-%d'
 
 #If rebooted, these times could be in the past in which case the cmd will error out but not cause problems.
-mycmd = 'echo python3 /home/pi/OutdoorLighting/LightsOff.py | at ' + sunriseLightsOffUtc.strftime(fmt)
-print(mycmd)
-os.system(mycmd)
+myCmd = myCmdLightsOff + ' | at ' + sunriseLightsOffUtc.strftime(fmt)
+print(myCmd)
+os.system(myCmdLightsOff + ' | at ' + sunriseLightsOffUtc.strftime(fmt))
 
-mycmd = 'echo python3 /home/pi/OutdoorLighting/LightsOn.py | at ' + sunsetLightsOnUtc.strftime(fmt)
-print(mycmd)
-os.system(mycmd)
+myCmd = myCmdLightsOn + ' | at ' + sunsetLightsOnUtc.strftime(fmt)
+os.system(myCmd)
